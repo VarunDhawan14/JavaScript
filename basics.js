302404ef5b5e7d7f1408bc9f31f83dba3b94dfec
Varun Dhawan <vddhawan9@gmail.com>
2026-01-27 18:02:43 +0530

    Starting with basics

--- basics.js
+++
@@ -1,7 +0,0 @@
-console.log("hey,hope your day will be beautiful !!");
-let name = "Varun Dhawan";
-const age = 20;
-
-console.log(name);
-console.log(age);
-console.log("This is the final Result");
