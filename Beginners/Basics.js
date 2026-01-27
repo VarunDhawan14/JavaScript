62739679f009322efd8443eec8f84e0d33d8cbb0
Varun Dhawan <vddhawan9@gmail.com>
2026-01-27 20:13:40 +0530

    Working on Javascript

---
+++ Beginners/Basics.js
@@ -0,0 +1,46 @@
+// ---- Arithimetic Operators in javascript ------
+
+// let number;
+// console.log(2 + 3 );      // addition
+// console.log(10 - 3 );     // subtraction
+// console.log(8 / 4 );      // division
+// console.log(3 % 2 );      // modulus
+// console.log(3 ** 2 );     // power
+
+// ---- Learn about booleans -------
+// --- False booleans list : null , undefined , 0 , -0 , false etc   ----
+
+// let istrue = true;
+// console.log(istrue);
+// let isfalse = false;
+// console.log(isfalse);
+
+// ------ Comparison Operators are of Two types ----------
+// 1. Relational operators 
+// 2. Equality operators 
+
+// 1. Relational operators are : > , < , <= , >= 
+
+// console.log( 10 > 10);   // false 
+// console.log( 10 >= 10);  // true 
+// console.log( 10 < 10);   // false 
+// console.log( 10 <= 10);  // true 
+
+// 2. Equality operators are : ( === ) strict equality {type + value} , (!==) strict not equality { type + value } , ( == ) lose equality {value} , (!= ) lose not equality
+//  we have to always use strict equality and strict not equality 
+
+// console.log( 20 === 20 );     // same data type and same value : strict equality
+// console.log( 30 !== 20);      // same data type and same value : strict not equality 
+// console.log( 30 == "30");     // different data type and same value : lose equality 
+// console.log( 40 != 40 );      // same value : lose not equality 
+
+
+
+
+
+
+
+
+
+
+
---
+++ Beginners/Challenges.js
@@ -0,0 +1,21 @@
+// ----- Perfoms some challenges C1 ------
+
+// const name = "Varun Dhawan";
+// let whatDoYouWannaBecomeinyourlife = "Programmer";
+// const gender = "Male" ;
+// console.log(name);
+// console.log(whatDoYouWannaBecomeinyourlife)
+// console.log(gender)
+
+// ----- Perfoms some challenges C2 ------
+
+// let firstnum = 50;
+// let secondnum = 30;
+
+// console.log(firstnum + secondnum);
+// console.log(firstnum - secondnum);
+// console.log(firstnum * secondnum);
+// console.log(firstnum / secondnum);
+// console.log(firstnum % secondnum);
+// console.log(firstnum ** secondnum);
+
