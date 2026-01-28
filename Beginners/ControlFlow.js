f269b66ef9af1d8810acb36b8fe7b60b832a6f81
Varun Dhawan <vddhawan9@gmail.com>
2026-01-28 23:58:40 +0530

    how conditional statement works

---
+++ Beginners/ControlFlow.js
@@ -0,0 +1,10 @@
+let number1 = 10;
+let number2 = 20;
+
+if ( number1 > number2){
+   console.log(" Number1 is greater than Number2 ");
+}else if ( number1 < number2){
+   console.log(" Number2 is greater than Number1 ");
+}else {
+   console.log(" Number1 is equals to Number2 ");
+}
