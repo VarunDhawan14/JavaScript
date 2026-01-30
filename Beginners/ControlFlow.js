8a0842654a07e6150e6056f43cb6b33a9b6a7a03
Varun Dhawan <vddhawan9@gmail.com>
2026-01-30 23:59:05 +0530

    Updating code

--- Beginners/ControlFlow.js
+++ Beginners/ControlFlow.js
@@ -1,10 +1,21 @@
-let number1 = 10;
-let number2 = 20;
+// let number1 = 10;
+// let number2 = 0;
 
-if ( number1 > number2){
-   console.log(" Number1 is greater than Number2 ");
-}else if ( number1 < number2){
-   console.log(" Number2 is greater than Number1 ");
-}else {
-   console.log(" Number1 is equals to Number2 ");
-}
+// if ( number1 > number2){
+//    console.log(" Number1 is greater than Number2 ");
+// }else if ( number1 < number2){
+//    console.log(" Number2 is greater than Number1 ");
+// }else {
+//    console.log(" Number1 is equals to Number2 ");
+// }
+
+
+let password ;
+
+if ( password <8){
+   console.log("Password is too short");
+}else if ( password >=8 ){
+   console.log("Welcome !!");
+}else{
+   console.log("Password is too long")
+};
