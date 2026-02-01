bf2cbcda5203e60e224eafe00b218d85b9e99205
Varun Dhawan <vddhawan9@gmail.com>
2026-02-01 23:37:26 +0530

    Learn about Javascript Logical operator

---
+++ Beginners/logicaloperator.js
@@ -0,0 +1,22 @@
+// ---- AND Logical operator ( && ) ------
+// ---- OR Logical operator (||) -----
+// --- NOT Logical operator (!) -----
+
+// let password = "Varun_Dhawan";
+
+// if (password.length >= 8 && password.includes("Dh")) {
+//   console.log("Valid Password");
+// } else {
+//   console.log("Invalid Password");
+// }
+
+// ---- Use of OR Operator ------
+
+// let number = 100;
+
+// if (number > 50 || number < 0) {
+//   console.log("Valid Number");
+// } else {
+//   console.log("Invalid Number");
+// }
+
