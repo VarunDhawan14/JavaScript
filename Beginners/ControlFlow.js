285b4f5973d2380f4ac929faabece3e85ab07e59
Varun Dhawan <vddhawan9@gmail.com>
2026-02-01 23:36:36 +0530

    Learn more about Conditional Statements

--- Beginners/ControlFlow.js
+++ Beginners/ControlFlow.js
@@ -1,6 +1,7 @@
+//  ----- IF ELSE IF ---- Conditional Statement -------
+
 // let number1 = 10;
 // let number2 = 0;
-
 // if ( number1 > number2){
 //    console.log(" Number1 is greater than Number2 ");
 // }else if ( number1 < number2){
@@ -9,13 +10,216 @@
 //    console.log(" Number1 is equals to Number2 ");
 // }
 
+// let password ;
 
-let password ;
+// if ( password <8){
+//    console.log("Password is too short");
+// }else if ( password >=8 ){
+//    console.log("Welcome !!");
+// }else{
+//    console.log("Password is too long")
+// };
 
-if ( password <8){
-   console.log("Password is too short");
-}else if ( password >=8 ){
-   console.log("Welcome !!");
-}else{
-   console.log("Password is too long")
-};
+// ----- SWITCH CASES ---------
+
+// let bulb = 20;
+
+// switch (bulb){
+//    case 0:
+//       console.log("OFF");
+//       break;
+//    case 1:
+//       console.log("ON");
+//       break;
+//    default:
+//       console.log("Bulb is not found");
+// }
+
+// --- another example on switch case -----
+
+// let day = "Monday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("Today is Monday");
+//     break;
+//   case "Tuesday":
+//     console.log("Today is Tuesday");
+//     break;
+//   case "Wednesday":
+//     console.log("Today is Wednesday");
+//     break;
+//   case "Thursday":
+//     console.log("Today is Thursday");
+//     break;
+//   case "Friday":
+//     console.log("Today is Friday");
+//     break;
+//   case "Saturday":
+//     console.log("Today is Saturday");
+//     break;
+//   case "Sunday":
+//     console.log("Today is Sunday");
+//     break;
+//   default:
+//     console.log("Wrong Number!!");
+// }
+
+// ----- FOR LOOP ----- how to use them ------
+
+// console.log("hey, Varun");
+//  ----- IF ELSE IF ---- Conditional Statement -------
+
+// let number1 = 10;
+// let number2 = 0;
+// if ( number1 > number2){
+//    console.log(" Number1 is greater than Number2 ");
+// }else if ( number1 < number2){
+//    console.log(" Number2 is greater than Number1 ");
+// }else {
+//    console.log(" Number1 is equals to Number2 ");
+// }
+
+// let password ;
+
+// if ( password <8){
+//    console.log("Password is too short");
+// }else if ( password >=8 ){
+//    console.log("Welcome !!");
+// }else{
+//    console.log("Password is too long")
+// };
+
+// ----- SWITCH CASES ---------
+
+// let bulb = 20;
+
+// switch (bulb){
+//    case 0:
+//       console.log("OFF");
+//       break;
+//    case 1:
+//       console.log("ON");
+//       break;
+//    default:
+//       console.log("Bulb is not found");
+// }
+
+// --- another example on switch case -----
+
+// let day = "Monday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("Today is Monday");
+//     break;
+//   case "Tuesday":
+//     console.log("Today is Tuesday");
+//     break;
+//   case "Wednesday":
+//     console.log("Today is Wednesday");
+//     break;
+//   case "Thursday":
+//     console.log("Today is Thursday");
+//     break;
+//   case "Friday":
+//     console.log("Today is Friday");
+//     break;
+//   case "Saturday":
+//     console.log("Today is Saturday");
+//     break;
+//   case "Sunday":
+//     console.log("Today is Sunday");
+//     break;
+//   default:
+//     console.log("Wrong Number!!");
+// }
+
+// ----- FOR LOOP ----- how to use them ------
+
+// console.log("hey, Varun");
+//  ----- IF ELSE IF ---- Conditional Statement -------
+
+// let number1 = 10;
+// let number2 = 0;
+// if ( number1 > number2){
+//    console.log(" Number1 is greater than Number2 ");
+// }else if ( number1 < number2){
+//    console.log(" Number2 is greater than Number1 ");
+// }else {
+//    console.log(" Number1 is equals to Number2 ");
+// }
+
+// let password ;
+
+// if ( password <8){
+//    console.log("Password is too short");
+// }else if ( password >=8 ){
+//    console.log("Welcome !!");
+// }else{
+//    console.log("Password is too long")
+// };
+
+// ----- SWITCH CASES ---------
+
+// let bulb = 20;
+
+// switch (bulb){
+//    case 0:
+//       console.log("OFF");
+//       break;
+//    case 1:
+//       console.log("ON");
+//       break;
+//    default:
+//       console.log("Bulb is not found");
+// }
+
+// --- another example on switch case -----
+
+// let day = "Monday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("Today is Monday");
+//     break;
+//   case "Tuesday":
+//     console.log("Today is Tuesday");
+//     break;
+//   case "Wednesday":
+//     console.log("Today is Wednesday");
+//     break;
+//   case "Thursday":
+//     console.log("Today is Thursday");
+//     break;
+//   case "Friday":
+//     console.log("Today is Friday");
+//     break;
+//   case "Saturday":
+//     console.log("Today is Saturday");
+//     break;
+//   case "Sunday":
+//     console.log("Today is Sunday");
+//     break;
+//   default:
+//     console.log("Wrong Number!!");
+// }
+
+// ----- FOR LOOP ----- how to use them ------
+
+// let name = "Varun Dhawan";
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(name, i);
+// }
+
+// ----- WHILE LOOP ----- basic syntax -----
+
+// let num = 0;
+
+// while (num <= 5) {
+//   console.log("HEY, you're beautiful");
+//   num++;
+// }
+
+
