477d79a040de77afaa8a7de288c30dfdc34005f0
Varun Dhawan <vddhawan9@gmail.com>
2026-02-03 22:50:34 +0530

    Function call & types

---
+++ Beginners/Functions.js
@@ -0,0 +1,38 @@
+// --- Basic Syntax for FUNCTIONS ----
+
+// function greet() {
+//   console.log("Good Morning babee");
+// }
+
+// greet();
+
+// ---- Use of PARAMETERS in Function -----
+
+// function myname(name) {
+//   console.log(`myname ${name}`);
+// }
+// myname("is Varun");
+
+//---- Use of RETURN statemenet in Function ----
+
+// function add(a, b) {
+//   return a + b;
+// }
+// let value = add(20, 20);
+// console.log(value);
+
+// ----- FUNCTION DECLARATION -----
+
+// function study(username) {
+//   console.log(`hey ${username}`);
+// }
+
+// study("varundhawan27");
+
+// ----- FUNCTION EXPRESSION -----
+
+// const greetings = function (user) {
+//   console.log(`hello ${user}`);
+// };
+
+// greetings("Good Morning !");
