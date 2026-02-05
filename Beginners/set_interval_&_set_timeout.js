67903893b574c760718d45591d87c5384073dc5a
Varun Dhawan <vddhawan9@gmail.com>
2026-02-05 16:35:38 +0530

    how to use setInterval & setTimeout

---
+++ Beginners/set_interval_&_set_timeout.js
@@ -0,0 +1,25 @@
+// ----- use of SET INTERVAL --------
+
+// setInterval(
+//   () => console.log(`this function is executing after every 2 seconds`),
+//   2000,
+// );
+
+// ---- use of SET TIMEOUT -------
+
+// setTimeout(function () {
+//   console.log(`hey cutie,what are you doing?`);
+// }, 1000);
+
+// ----- Clear Interval -----
+
+// const Interval_id = setInterval(function () {
+//   console.log(`This function is being executed at every 1 second`);
+// }, 1000);
+
+// // ----- stop the interval after 5 seconds -----
+
+// setTimeout(function () {
+//   clearInterval(Interval_id);
+//   console.log(`Interval Stopped`);
+// }, 5000);
