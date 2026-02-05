8ecd91fe2fef1b85833434ad259d0edfa633f0da
Varun Dhawan <vddhawan9@gmail.com>
2026-02-05 16:36:26 +0530

    how to set and check date & time

--- Beginners/Date_Time.js
+++ Beginners/Date_Time.js
@@ -1,2 +1,44 @@
-const current_date = new Date(2026, 1, 5, 6, 30, 0, 0);
-console.log(current_date);
+// ------ Current Date and time checking -----
+
+// const current_date = Date(2026, 1, 5, 15, 10, 0, 0);
+// console.log(current_date);
+
+// ----- checking any date -----
+
+// const anydate = new Date(2026, 1, 15, 1, 20, 0, 0);
+// console.log(anydate);
+
+// ----- useful for extracting Date and Time -----
+
+const date = new Date();
+
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDay();
+// const hours = date.getHours();
+// const minutes = date.getMinutes();
+// const seconds = date.getSeconds();
+// const milliseconds = date.getMilliseconds();
+
+// console.log(`year : ${year}`);
+// console.log(`month : ${month}`);
+// console.log(`day : ${day}`);
+// console.log(`hours : ${hours}`);
+// console.log(`minutes : ${minutes}`);
+// console.log(`seconds : ${seconds}`);
+// console.log(`milliseconds : ${milliseconds}`);
+
+// ----- different kind of formats to use date and time -----
+
+// const date = new Date();
+
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toISOString());
+// console.log(date.getDay());
+
+// --- to increment or update the date ------
+
+// date.setDate(date.getDate() + 10);
+// console.log(date.toDateString());
+
