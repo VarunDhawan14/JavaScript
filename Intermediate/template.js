92b7de3a881b92980ce92573aedcc027929ee899
Varun Dhawan <vddhawan9@gmail.com>
2026-02-06 23:36:03 +0530

    use of template strings with variables

--- Intermediate/template.js
+++ Intermediate/template.js
@@ -1,1 +1,23 @@
-console.log(`hey cutie`);
+// ------ ${} is the lateral template string to get output in the form of backtick `` -------
+
+// let firstname = "Varun";
+// let secondname = "Dhawan";
+
+// console.log(`${firstname} ${secondname}`);
+
+//--- access it using function -----
+
+// function info() {
+//   return `I am pursuing bachelors from Thapar University Patiala`;
+// }
+
+// console.log(`${info()}`);
+
+// ---- template using input variables ----
+
+let name = "Varun Dhawan";
+let age = 20;
+let branch = "ENC";
+
+console.log(`hey, my name is ${name} & my branch is ${name}`);
+console.log(`I am pursuing bachelors from Thapar University Patiala`);
