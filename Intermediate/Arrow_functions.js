fdd05c2d609c728bdd8668a9c769f5c607e8bd01
Varun Dhawan <vddhawan9@gmail.com>
2026-02-09 23:58:21 +0530

    how to work on arrow functions

---
+++ Intermediate/Arrow_functions.js
@@ -0,0 +1,13 @@
+// ---- Normal Functions ------
+
+function greet() {
+  console.log(`hey,good morning!!`);
+}
+greet();
+
+// ---- Arrow Functions, also known as fat arrow functions,are a concise and shorter way to define functions in javascript. They were introduced in ECMAscript 6 (ES6) and provide a more compact syntax compared to ttraditional function expressions.
+
+asking = () => {
+  console.log(`what are you doing?`);
+};
+asking();
