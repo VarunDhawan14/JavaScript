e755ea75652168f8a00787f5d3170010036a9026
Varun Dhawan <vddhawan9@gmail.com>
2026-02-10 13:57:20 +0530

    Defining objects via ES6

--- Intermediate/Arrow_functions.js
+++ Intermediate/Arrow_functions.js
@@ -1,13 +1,23 @@
 // ---- Normal Functions ------
 
-function greet() {
-  console.log(`hey,good morning!!`);
-}
-greet();
+// function greet() {
+//   console.log(`hey,good morning!!`);
+// }
+// greet();
 
 // ---- Arrow Functions, also known as fat arrow functions,are a concise and shorter way to define functions in javascript. They were introduced in ECMAscript 6 (ES6) and provide a more compact syntax compared to ttraditional function expressions.
 
-asking = () => {
-  console.log(`what are you doing?`);
-};
-asking();
+// asking = () => {
+//   console.log(`what are you doing?`);
+// };
+// asking();
+
+// ---- Arrow Functions, with variable name ----
+
+// const greet = (username) => `hey,good morning babe!`;
+// console.log(greet());
+
+// const double = (number) => number * 2;
+// console.log(`Number after double is :`, double(40));
+
+
---
+++ Intermediate/enhanced_object.js
@@ -0,0 +1,10 @@
+// --- Enhanced object literals, introudced in ECMAScript 6 (ES6), are a set of ehancements to the syntax for defining objects in javascript. These enhancements make it more convenient and concise to define object properties and methods -----
+
+function user(name, age, work) {
+  return {
+    name,
+    age,
+    work,
+  };
+}
+console.log(user("Varun Dhawan", 20, "Student & Programmer"));
