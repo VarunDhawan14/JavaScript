823dd7db4fe447678a400ddb189ca17ae19f6dd3
Varun Dhawan <vddhawan9@gmail.com>
2026-02-12 23:40:55 +0530

    how to work on default parameters

---
+++ Intermediate/Default_parameters.js
@@ -0,0 +1,4 @@
+// ---- Default Parameters : Default function parameters, introduced in ECMAScript 6 (ES6), allow you to assign default values to function parameters. When a function is called, and an argument is not provided for a paramter, the default value will be used instead. This feature makes it easier to handle cases where arguments might be missing or undefined.
+
+
+
