987cea9d9133a20f0f077b03629a54be8d491ad6
Varun Dhawan <vddhawan9@gmail.com>
2026-02-16 23:59:53 +0530

    added new file

---
+++ Intermediate/spread_operator.js
@@ -0,0 +1,5 @@
+// ---- The spread operator is a new addition to the set of operator in Javascript ES6.
+// It takes in an iterable (e.g an array) and expands it into individual elements.
+// The spread operator it commonly used to make shallow copies of JS objects.
+// Using this operator makes the code concise and enhances its readability.
+
