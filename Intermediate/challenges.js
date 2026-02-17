b14d340f827843fff4ce02c89e07b8dc5c323d6e
Varun Dhawan <vddhawan9@gmail.com>
2026-02-17 23:52:26 +0530

    modify the file

--- Intermediate/challenges.js
+++ Intermediate/challenges.js
@@ -1,6 +1,8 @@
 // ----- Challenge 1 ---- topic : Default Parameters
 
-function multiply(a, b = 1) {
-  return a * b;
-}
-console.log(multiply(5));
+// function multiply(a, b = 1) {
+//   return a * b;
+// }
+// console.log(multiply(5));
+
+
--- Intermediate/spread_operator.js
+++ Intermediate/spread_operator.js
@@ -3,3 +3,13 @@
 // The spread operator it commonly used to make shallow copies of JS objects.
 // Using this operator makes the code concise and enhances its readability.
 
+function giveMe4(a, b, c, d) {
+  console.log("a : ", a);
+  console.log("b : ", a);
+  console.log("c : ", a);
+  console.log("d : ", a);
+}
+
+const colors = ["red", "yellow", "blue", "green"];
+
+giveMe4(colors);
