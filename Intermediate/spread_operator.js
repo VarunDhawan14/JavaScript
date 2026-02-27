092e4ac9f940f0381e072dfd152d7a4bbb3238fb
Varun Dhawan <vddhawan9@gmail.com>
2026-02-27 23:33:33 +0530

    how to use rest operator in function paramters

--- Intermediate/spread_operator.js
+++ Intermediate/spread_operator.js
@@ -28,15 +28,19 @@
 
 // ----- spread operator using OBJECT -----
 
-const obj1 = { x: 1, y: 2, z: 3 };
-const obj2 = { a: 5, b: 10 };
-const obj3 = { ...obj1, ...obj2 };
+// const obj1 = { x: 1, y: 2, z: 3 };
+// const obj2 = { a: 5, b: 10 };
+// const obj3 = { ...obj1, ...obj2 };
 
-console.log(...obj3);
+// console.log(...obj3);
 
 // ------------  REST OPERATOR   --------
 
 // ---The rest parameter syntax allows a function to accept an indefinite number of arguments.
 // ---as an array, providing a way to represent variadic functions in javascript
 
-
+function user(x, ...userdata) {
+  console.log(x);
+  console.log(userdata);
+}
+user("Varun Dhawan", 20, "programmer", "learning and growing");
