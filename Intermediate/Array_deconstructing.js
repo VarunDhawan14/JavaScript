9cd2909a0aa7bc8726ae544e2bff1381e006be56
Varun Dhawan <vddhawan9@gmail.com>
2026-03-02 23:41:15 +0530

    how to use deconstruction via arrays & objects

---
+++ Intermediate/Array_deconstructing.js
@@ -0,0 +1,16 @@
+// -- Deconstructing allows us to "unpack" values from data-structures (Arrays,objects) into separate distinct variables.
+
+// ------ Syntax for deconstructing by ARRAYS ----
+
+// const number = ["one", "two", "three"];
+
+// const [varun, dhawan, home, college] = number;
+// console.log(varun);
+// console.log(college);
+
+// /---- another example -----
+
+let a, b;
+
+[a = 30, b = 20] = ["one", "two"];
+console.log(a);
