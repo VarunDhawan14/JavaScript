eba5affb4b5fcf0fcedf7a0b464ac45b2bca456a
Varun Dhawan <vddhawan9@gmail.com>
2026-04-01 23:41:57 +0530

    Created new file and did some modifications

---
+++ Intermediate/Functions.js
@@ -0,0 +1,17 @@
+// ----- Basic Use of Functions ------
+
+// function sum(x, y) {
+//   add = x + y;
+//   console.log("Value after addition : ");
+//   return add;
+// }
+
+// const value = sum(20, 30);
+// console.log(value);
+
+// --- use of arrow functions in javascript -----
+
+const arrowsum = (a, b) => {
+  console.log(a + b);
+};
+arrowsum(3, 4);
