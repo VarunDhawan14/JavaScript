97f34b3d9d5ef5ded21ed9f75256aeed47dbb66f
Varun Dhawan <vddhawan9@gmail.com>
2026-04-04 23:51:07 +0530

    Added new challenge of functions

--- Intermediate/challenges.js
+++ Intermediate/challenges.js
@@ -14,9 +14,13 @@
 // ---- challenge as Function ----
 // --- Ques : Create a function using the "function" keyword that takes a string as an argument and returns the number of vowels in the string.
 
-function string1(str) {
-  console.log("varundhawan");
-  return str;
+// function string1(str) {
+//   console.log("varundhawan");
+//   return str;
+// }
+// const value = string1("programmer");
+// console.log(value);
+
+function numbers(num) {
+  console.log("This is the final output");
 }
-const value = string1("programmer");
-console.log(value);
