f7e22f8c1921cc7c42b8531e213ae37ff8fd636c
Varun Dhawan <vddhawan9@gmail.com>
2026-06-05 23:39:53 +0530

    learned about DOM Manipulation and modified it

--- Advanced/DOM_part1/script.js
+++ Advanced/DOM_part1/script.js
@@ -1,2 +1,29 @@
-let elements = document.querySelectorAll(".myclass");
-console.log(elements);
+// let elements = document.querySelectorAll(".myclass");
+// console.log(elements)
+
+// let finalvalue = document.querySelector(".myclass");
+// console.log(finalvalue)
+
+// let allclasses = document.querySelectorAll(".myclass")
+// console.log(allclasses)
+
+// let button = document.querySelector("#myid2")
+// console.log(button)
+
+// let inner = document.querySelector("div")
+// console.log(inner)
+
+// let value = document.querySelector("p")   
+// console.log(value.getAttribute("class"))
+
+// let value = document.querySelector("id")   
+// console.log(value.getAttribute("id"))
+
+// let value = document.querySelector("p")   
+// console.log(value.getAttribute("class"))
+
+// let value = document.querySelector("p")
+// console.log(value.getAttribute("class"))
+
+// let newElement = document.createElement("div")
+// console.log(newElement.setAttribute("id","newID"))
