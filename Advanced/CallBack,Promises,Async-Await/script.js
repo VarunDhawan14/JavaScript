2fd3f790b0453c96af72e3d231de4afed255f0bc
Varun Dhawan <vddhawan9@gmail.com>
2026-06-16 21:34:02 +0530

    completed Js till promises

--- Advanced/CallBack,Promises,Async-Await/script.js
+++ Advanced/CallBack,Promises,Async-Await/script.js
@@ -40,19 +40,67 @@
 // let promise = new Promise((resolve,reject) => {........})
 // -- there are three states in Promises : "Pending" , "Fulfilled" , "Rejected"
 
-let promise = new Promise((resolve, reject) => {
-  console.log("hey what's going?");
-  resolve("code execution successful");
-});
+// let promise = new Promise((resolve, reject) => {
+//   console.log("hey what's going?");
+//   resolve("code execution successful");
+// });
 
-function getData(dataID, getNextData) {
-  setTimeout(() => {
-    console.log("data :", dataID);
-  }, 5000);
-}
-getData("123", () => {
-  console.log("this is next data");
-  getData("456", () => {
-    console.log("this is another data");
-  });
-});
+// function getData(dataID, getNextData) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       // console.log("data :", dataID);
+//       // resolve("Success");
+//       reject("error");
+//       if (getNextData) {
+//         getNextData();
+//       }
+//     }, 5000);
+//   });
+// }
+// let result = getData("123");
+
+// ----- Use of Promise then and Catch -------------------------------------------------
+
+// const getpromise = () => {
+//   return new Promise((resolve, reject) => {
+//     console.log("I am Varun Dhawan");
+//     resolve("Code Executed");
+//   });
+// };
+
+// let finaloutput = getpromise();
+// finaloutput
+//   .then((result) => {
+//     console.log("Promise fulfilled", result);
+//   })
+//   .catch((err) => {
+//     console.log("Proimse rejected", err);
+//   });
+
+// ------------ Use of Promise Chains -------------------
+
+// function asyncfunc1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("some data1");
+//       resolve("Successfully fulfilled");
+//     }, 4000);
+//   });
+// }
+// function asyncfunc2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Data 2");
+//     }, 4000);
+//   });
+// }
+// console.log("fetching data1.....");
+// asyncfunc1().then((result) => {
+//   console.log("fetching data2......");
+//   asyncfunc2().then((result) => {});
+// });
+
+
+
+
+
